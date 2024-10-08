136d22fa6db82a424adf1bb795c8991ae0d12026
T <1727648092@qq.com>
2024-10-09 00:37:40 +1100

    package.json change

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
-const mysql = require('mysql2');
-
-// 配置 MySQL 连接
-const db = mysql.createConnection({
-    host: '34.129.180.29',  // 使用你的数据库hostname
-    user: 'root',            // 替换为你的MySQL用户名
-    password: 'taskme103',    // 替换为你的MySQL密码
-    database: 'Character' // 替换为你的数据库名称
-});
-
-// 连接数据库
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err);
-        return;
-    }
-    console.log('Connected to MySQL');
-});
-
-app.use(bodyParser.json());
 
 // 定义路由，响应根路径请求
 app.get('/', (req, res) => {
