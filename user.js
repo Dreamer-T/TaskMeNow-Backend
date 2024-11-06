79219a90dff0b8c684092e069650f79617daff65
unknown <1727648092@qq.com>
2024-11-06 22:17:30 +0800

    result of group checking now shows group nameg

--- user.js
+++ user.js
@@ -19,7 +19,14 @@ router.get('/id/groups/:id', async (req, res) => {
     const id = req.params.id;
     try {
         const groups = await getGroupsFromDB('SELECT * FROM GroupAndUser WHERE userID = ?;', [id]);
-        res.status(200).json(groups);
+
+        const groupIds = groups.map(group => group.GroupID);
+
+        // 第二次查询，根据 GroupID 查询更多的组信息
+        const placeholders = groupIds.map(() => '?').join(',');
+        const groupDetailsQuery = `SELECT * FROM GroupTypes WHERE ID IN (${placeholders})`;
+        const groupDetails = await getGroupsFromDB(groupDetailsQuery, groupIds);
+        res.status(200).json(groupDetails);
     } catch (error) {
         console.error('Error fetching task:', error);
         res.status(500).json({ error: 'Database query error' });
