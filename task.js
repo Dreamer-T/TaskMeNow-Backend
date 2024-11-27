64317568aeef423e10ebef09caff49da2453da1b
T <1727648092@qq.com>
2024-11-27 18:07:17 +1100

    add update/reassign task api, change create task api

--- task.js
+++ task.js
@@ -52,18 +52,19 @@ router.get('/assignedTo/:assignedTo', authorizeRole('Staff'), async (req, res) =
 
 
 
-// API of create task, at least to be a staff
+// API of create a task, at least to be a staff
 router.post('/createTask', authorizeRole('Staff'), async (req, res) => {
-    const { taskDescription, taskImage, assignedTo, createdBy, urgencyLevel } = req.body;
+    const { taskDescription, taskImage, assignedTo, createdBy, urgencyLevel, tags } = req.body;
 
     // check those are necessary
-    if (!taskDescription || !assignedTo || !createdBy || !urgencyLevel) {
+    if (!taskDescription || !assignedTo || !createdBy || !urgencyLevel || !tags) {
         return res.status(400).json({ error: 'Missing required fields' });
     }
 
     try {
-        const query = 'INSERT INTO Tasks (taskDescription, taskImage, assignedTo, createdBy, urgencyLevel) VALUES (?, ?, ?, ?, ?)';
-        const values = [taskDescription, taskImage || null, assignedTo, createdBy, urgencyLevel];
+        var jsonTagIDs = JSON.stringify(tags);
+        const query = 'INSERT INTO Tasks (taskDescription, taskImage, assignedTo, createdBy, urgencyLevel, tags) VALUES (?, ?, ?, ?, ?, ?)';
+        const values = [taskDescription, taskImage || null, assignedTo, createdBy, urgencyLevel, jsonTagIDs];
         const result = await SQLExecutor(query, values);
 
         res.status(200).json({ message: 'Task created successfully', taskId: result.insertId });
@@ -73,6 +74,49 @@ router.post('/createTask', authorizeRole('Staff'), async (req, res) => {
     }
 });
 
+
+// API of update a task, at least to be a staff
+router.post('/updateTask', authorizeRole('Staff'), async (req, res) => {
+    const { taskID, taskDescription, taskImage, urgencyLevel, tags } = req.body;
+
+    // check those are necessary
+    if (!taskID || !taskDescription || !urgencyLevel || !tags) {
+        return res.status(400).json({ error: 'Missing required fields' });
+    }
+
+    try {
+        var jsonTagIDs = JSON.stringify(tags);
+        const query = 'UPDATE Tasks SET taskDescription = ?, taskImage = ?, urgencyLevel = ?, tags = ? WHERE ID = ?';
+        const values = [taskDescription, taskImage || null, urgencyLevel, jsonTagIDs, taskID];
+        const result = await SQLExecutor(query, values);
+
+        res.status(200).json({ message: 'Task updated successfully', affectedRowNumber: result.affectedRows });
+    } catch (error) {
+        console.error('Error updating task:', error);
+        res.status(500).json({ error: 'Database error' });
+    }
+});
+
+// API of reassign a task, at least to be a staff
+router.post('/reassignTask', authorizeRole('Supervisor'), async (req, res) => {
+    const { taskID, assignedTo } = req.body;
+
+    // check those are necessary
+    if (!taskID || !assignedTo) {
+        return res.status(400).json({ error: 'Missing required fields' });
+    }
+
+    try {
+        const query = 'UPDATE Tasks SET assignedTo = ? WHERE ID = ?';
+        const values = [assignedTo, taskID];
+        const result = await SQLExecutor(query, values);
+
+        res.status(200).json({ message: 'Task reassigned successfully', affectedRowNumber: result.affectedRows });
+    } catch (error) {
+        console.error('Error reassigning task:', error);
+        res.status(500).json({ error: 'Database error' });
+    }
+});
 
 // API of complete task, at least to be a staff
 router.post('/completeTask', authorizeRole('Staff'), async (req, res) => {
@@ -95,6 +139,7 @@ router.post('/completeTask', authorizeRole('Staff'), async (req, res) => {
     }
 });
 
+// API for supervisor and manager to get all tasks
 router.get('/allTask', authorizeRole('Supervisor'), async (req, res) => {
     try {
         const task = await SQLExecutor('SELECT * FROM Tasks;', []);
@@ -104,5 +149,7 @@ router.get('/allTask', authorizeRole('Supervisor'), async (req, res) => {
         res.status(500).json({ error: 'Database query error' });
     }
 });
+
+
 
 module.exports = router;
