ec51119c92236e87ec039f84865a9c8eb80266a9
T <1727648092@qq.com>
2024-12-26 12:14:13 +1100

    update login logic, add login count as a field to show tips

--- auth.js
+++ auth.js
@@ -19,6 +19,18 @@ const getTagsFromDB = async (id) => {
     }
 };
 
+const SQLExecutor = async (query, params = []) => {
+    const pool = getPool();
+    const conn = await pool.getConnection();
+    try {
+        console.log(query);
+        const [result] = await conn.query(query, params);
+        return result;
+    } finally {
+        await conn.release();
+    }
+};
+
 // user login
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
@@ -34,9 +46,10 @@ router.post('/login', async (req, res) => {
         if (!isValidPassword) {
             return res.status(400).json({ error: 'Invalid email or password' });
         }
-
+        const loginTimes = await SQLExecutor('SELECT loginTimes FROM Users WHERE ID = ?', [user.ID]);
+        SQLExecutor('UPDATE Users SET loginTimes = ? WHERE ID = ?', [loginTimes[0].loginTimes + 1, user.ID]);
         // JWT
-        const token = jwt.sign({ id: user.ID, email: user.email, role: user.userRole }, JWT_SECRET, { expiresIn: '1h' }); // 返回令牌和用户信息
+        const token = jwt.sign({ id: user.ID, email: user.email, role: user.userRole }, JWT_SECRET, { expiresIn: '7d', 'loginTimes': loginTimes + 1 }); // 返回令牌和用户信息
         const tagReult = getTagsFromDB(user.ID);
         res.status(200).json({
             message: '登录成功',
@@ -49,9 +62,10 @@ router.post('/login', async (req, res) => {
                 createdTime: user.createdTime,
                 'token': token,
                 // 这里还需要修改，添加查询Tag的操作，通过查询Tags来得到TagID
-                tags: tagReult.tagID
+                tags: tagReult.tagID,
+                'loginTimes': loginTimes + 1
             },
-            expiresIn: 3600  // 告知客户端令牌的过期时间
+            expiresIn: 7 * 24 * 60 * 60
         })
     } catch (error) {
         console.error('Error during login:', error);
